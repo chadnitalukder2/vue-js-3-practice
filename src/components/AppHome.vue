544d05716489ca5de02ee432237682e8beae6e20
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-03 01:21:26 +0600

    v-bind

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,33 +1,15 @@
 <template>
- <h1 @mousemove="getConsole()">Home Component</h1>
- <button @click="getData('button 1 click')">click me</button>
- <button v-on:dblclick="getData('button 2 click')">click me</button><br>
- 
-<h2>{{ count }}</h2>
-<button @click="countIAdd">+1</button>
-<button @click="countIRe">-1</button>
+  <h2>Two Way Binding</h2>
+  <input v-model="data" type="text" />
+  <h2>{{ data }}</h2>
 </template>
 <script>
 export default {
   name: "AppHome",
-   data(){
-    return{
-        count: 0
-    }
-   },
-  methods: {
-    getData(data) {
-     alert (data)
-    },
-    getConsole(){
-        console.log("event is fired");
-    },
-    countIAdd(){
-       this.count = this.count +1
-    },
-    countIRe(){
-        this.count = this.count -1
-    }
+  data() {
+    return {
+      data: "",
+    };
   },
 };
 </script>
