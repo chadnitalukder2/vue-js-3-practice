119c0b7409d38440f2210484e4e8782453a19fb7
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-02 20:20:41 +0600

    component

---
+++ src/components/AppHome.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Email : {{ email }}</h1>
+    <h1> Mobile : {{ mobile = 999 }}</h1>
+<h1> Mobile : {{ mobile}}</h1>
+<h1>Name : {{ getName("Chadni talukder") }} </h1>
+</template>
+<script>
+export default {
+  name: "AppHome",
+ data(){
+    return{
+        email: "puja@124test.com",
+        mobile: "01753507283",
+        getName(x){
+            return (x)
+        }
+    }
+ }
+}
+</script>
+
+<style scoped>
+h1{
+    color: orange;
+}
+</style>
+
