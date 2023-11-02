9af8120fd0604f0ca3bf7bd6515f67ec360dd002
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-03 01:06:57 +0600

    Event

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,34 +1,39 @@
 <template>
-    <h1>Email : {{ email }}</h1>
-   <h1>Name : {{ getName("puja das") }} </h1>
-   <h1>Data : {{ getData().email }} </h1>
+ <h1 @mousemove="getConsole()">Home Component</h1>
+ <button @click="getData('button 1 click')">click me</button>
+ <button v-on:dblclick="getData('button 2 click')">click me</button><br>
+ 
+<h2>{{ count }}</h2>
+<button @click="countIAdd">+1</button>
+<button @click="countIRe">-1</button>
 </template>
 <script>
 export default {
   name: "AppHome",
- data(){
+   data(){
     return{
-        email: "puja@124test.com",
-      
+        count: 0
     }
- },
- methods: {
-    getName(name){
-            return (name);
-        },
-        getData(){
-            return{
-                name: "chadni talukder",
-                email: this.email
-            }
-        }
- }
-}
+   },
+  methods: {
+    getData(data) {
+     alert (data)
+    },
+    getConsole(){
+        console.log("event is fired");
+    },
+    countIAdd(){
+       this.count = this.count +1
+    },
+    countIRe(){
+        this.count = this.count -1
+    }
+  },
+};
 </script>
 
 <style scoped>
-h1{
-    color: orange;
+h1 {
+  color: orange;
 }
 </style>
-
