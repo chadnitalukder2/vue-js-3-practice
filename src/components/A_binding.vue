922ba9d85bc4c8a6a0f02197f0893374f782942f
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-05 10:51:49 +0600

    check-box

--- src/components/A_binding.vue
+++ src/components/A_binding.vue
@@ -1,40 +1,48 @@
 <template>
-    <div>
-      <h1>Get input field value</h1>
-      <label>Email: </label>
-      <input type="text" v-model="email" placeholder="enter email"><br><br>
+<div>
+    <h1>Get checkbox & radio button value</h1>
+    <h3>Technology</h3>
+ 
+    <input type="checkbox" v-model="technology" value="java" id="java">
+    <label for="java">Java</label>
+    <input type="checkbox" v-model="technology" value="Node" id="Node">
+    <label for="Node">Node</label>
+    <input type="checkbox" v-model="technology" value="PHP" id="PHP">
+    <label for="PHP">PHP</label>
+    <br><br>
+    <h1>Profession</h1>
+    <input type="radio" v-model="Profession"  value="student" id="student" name="Profession">
+    <label for="student">Student</label>
+    <input type="radio" v-model="Profession"  value="developer" id="developer" name="Profession">
+    <label for="developer">developer</label>
+
+    <h4>Technology : {{ technology }}</h4>
+   <h4>I am a : {{ Profession }}</h4>
+</div>
+</template>
+
   
-      <label>Password: </label>
-      <input type="password" v-model="password" placeholder="enter password"><br><br>
-  
-      <button type="button" @click.prevent="getData">Get Data</button>
-  
-      <h1>Email: {{ email }}</h1>
-      <h1>Password: {{ password }}</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        email: "",
-        password: ""
-      }
+        return {
+            technology: [],
+            Profession: null,
+        }
     },
     methods: {
-      getData() {
-        // You can access the values entered in the input fields using this.email and this.password
-        // Update the displayed values
-      console.log("values : " , this.email , this.password);
-      }
-    }
-  }
-  </script>
+        getData() {
+
+            console.log("values : ", this.email, this.password);
+        }
+    },
+
+}
+</script>
+
   
-  <style scoped>
-  h1 {
+<style scoped>
+h1 {
     color: orange;
-  }
-  </style>
-  
+}
+</style>
