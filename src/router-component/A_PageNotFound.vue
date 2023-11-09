f73540f5146f1b597e8bfeebcf283f90df34f720
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-09 12:08:05 +0600

    404 page

---
+++ src/router-component/A_PageNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1> 404 Page</h1>
+</template>
+
+<script>
+    export default {
+        name:"A_PageNotFound"
+    }
+</script>
+
+<style lang="scss" scoped>
+h1{
+    color: orange;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import routerHome from './router-component/A_routHome.vue';
 import Profile from './router-component/A_profile.vue';
 import Login from './router-component/A_login.vue';
 
+import PageNotFound from './router-component/A_PageNotFound.vue';
+
 const routes=[
     {
         name: "A_routHome",
@@ -11,13 +13,18 @@ const routes=[
     },
     {
         name: "profile",
-        path:'/profile/:name/:id',
+        path:'/profile/:name',
         component: Profile
      },
     {
         name: "login",
         path:'/avbhbhuhjj/uhjij',
         component: Login
+    },
+    {
+        name: "NotFound",
+        path:'/:pathMatch(.*)*',
+        component: PageNotFound,
     },
 ];
 const router = createRouter({
